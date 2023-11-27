0aede0f87079f25e5b5547d29188fdb5fdbff58d
yunk._.k <hamndo@naver.com>
2023-11-27 16:40:20 +0900

    같은 숫자는 싫어

---
+++ stack&queue/12906.js
@@ -0,0 +1,14 @@
+// 같은 숫자는 싫어
+
+function solution(arr) {
+  let answer = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    if (answer[answer.length - 1] !== arr[i]) {
+      answer.push(arr[i]);
+    }
+  }
+
+  return answer;
+}
+
+// filter 써보기!
